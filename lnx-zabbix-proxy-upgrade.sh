c7198cfc2e56406c0014febb184967cef9be9caa
cohatom <cohat337@gmail.com>
2022-04-21 11:21:12 +0200

    check mariadb version at the end

--- lnx-zabbix-proxy-upgrade.sh
+++ lnx-zabbix-proxy-upgrade.sh
@@ -1,11 +1,7 @@
-mysql -uroot
-SET GLOBAL innodb_fast_shutdown = 1;
-XA RECOVER;
-quit
+mysql -uroot -Bse "SET GLOBAL innodb_fast_shutdown = 1;XA RECOVER;"
 
 sudo systemctl stop mariadb
 sudo systemctl stop zabbix-proxy
-
 
 sudo apt-get remove "mariadb-*" -y
 
@@ -23,6 +19,12 @@ sudo ./mariadb_repo_setup --mariadb-server-version="mariadb-10.5"
 
 sudo apt update
 
-sudo apt install mariadb-serve-10.5
+sudo apt install mariadb-server-10.5 -y
 
 sudo mariadb-upgrade --force
+
+VERSION=$(mysql -uroot -Bse "SELECT VERSION();")
+ISVERSION='10.5'
+if [[ "$VERSION" == *"$ISVERSION"* ]]; then
+  echo "MariaDB updated and running!"
+fi
