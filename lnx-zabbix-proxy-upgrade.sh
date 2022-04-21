d85b5ea207b246b081b9a62c8c1a031ade467d4c
cohatom <cohat337@gmail.com>
2022-04-21 10:52:10 +0200

    initial commit

---
+++ lnx-zabbix-proxy-upgrade.sh
@@ -0,0 +1,26 @@
+mysql -uroot
+SET GLOBAL innodb_fast_shutdown = 1;
+XA RECOVER;
+quit
+
+sudo systemctl stop mariadb
+
+sudo apt-get remove "mariadb-*" -y
+
+sudo apt remove galera-3 -y
+
+sudo apt install wget
+
+wget https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
+
+echo "b9e90cde27affc2a44f9fc60e302ccfcacf71f4ae02071f30d570e6048c28597 mariadb_repo_setup" | sha256sum -c -
+
+chmod +x mariadb_repo_setup
+
+sudo ./mariadb_repo_setup --mariadb-server-version="mariadb-10.5"
+
+sudo apt update
+
+sudo apt install mariadb-serve-10.5
+
+sudo mariadb-upgrade --force
